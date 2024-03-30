9ec2ae5fae1bd6a09904c620e5c1f0aafdab5121
maryam <shabanzadeh@yahoo.com>
2024-03-30 19:31:59 +0100

    handel error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 import Axios from "axios";
 import GridLayout from "./Components/grid";
 
 export const mapContext = createContext();
 
 function App() {
-  const [inputValue, setInputValue] = useState(" ");
-  const [latitude, setLatitude] = useState(" ");
-  const [longitude, setLongitude] = useState(" ");
-  const [fetchResult, setFetchResult] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [fetchResult, setFetchResult] = useState(null);
+  const [error, setError] = useState(null);
 
   const fetchIP = () => {
-    Axios.get(`https://ipinfo.io/${inputValue}/json`).then((res) => {
-      console.log(res.data);
-      setInputValue(res.data.ip);
-      const { loc } = res.data;
-      const [latitude, longitude] = loc.split(",");
-      setLatitude(latitude);
-      setLongitude(longitude);
-      setFetchResult(res.data);
-    });
+    Axios.get(`https://ipinfo.io/${inputValue}/json`)
+      .then((res) => {
+        console.log(res.data);
+        setInputValue(res.data?.ip);
+        const { loc } = res.data;
+        const [latitude, longitude] = loc.split(",");
+        setLatitude(latitude);
+        setLongitude(longitude);
+        setFetchResult(res.data);
+        setError(null); 
+      })
+      .catch((error) => {
+        console.error("Error fetching IP information:", error);
+        setError("Failed to fetch IP information. Please try again.");
+      });
   };
 
   return (
@@ -31,6 +38,7 @@ function App() {
         fetchResult,
         latitude,
         longitude,
+        error,
       }}
     >
       <div>
